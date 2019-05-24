42a01aa3e17c16a8e2f2ca6469337cd870747dda
KushnerykPavel <levelup.kpi@gmail.com>
2019-05-24 14:48:07 +0300

    add nginx

--- static/js/movie-loader.js
+++ static/js/movie-loader.js
@@ -8,9 +8,7 @@ function getinfo(movie_id, title, api_key) {
              {
                  var image_url = 'http://image.tmdb.org/t/p/w500/'
                             + result.movie_results[0].poster_path
-                            console.log(image_url,  $idx_image_src, $('#'+movie_id+' img'))
                  var $idx_image_src = $('#'+movie_id+' img').attr('src', image_url)
-                 console.log(image_url,  $idx_image_src, $('#'+movie_id+' img'))
             }
         }
     )
@@ -25,18 +23,17 @@ function get_movie_data(movie_id, title, api_key) {
              {
                  var image_url = 'http://image.tmdb.org/t/p/w500/'
                             + result.movie_results[0].poster_path
-                 console.log(result.movie_results)
-                 // result.movie_results[0].overview
                  var $idx_image_src = $('.single-prd-item img').attr('src', image_url)
                  var $description = $('div#home p').append(result.movie_results[0].overview)
                  var $popularity = $('#popularity').append(result.movie_results[0].popularity)
-                 console.log($description)
+
             }
         }
     )
 }
 
 function add_impression(user_id, event_type, content_id, session_id, csrf_token) {
+            console.log('111111');
             $.ajax({
                  type: 'POST',
                  url: '/collect/log/',
