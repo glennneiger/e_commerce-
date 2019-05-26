6af4baa1744769b5d8e86d7dcffa4d566d185dff
KushnerykPavel <levelup.kpi@gmail.com>
2019-05-26 12:07:56 +0300

    add alerting and set cart data to localstorage

---
+++ static/js/collector.js
@@ -0,0 +1,16 @@
+function add_impression(user_id, event_type, content_id, session_id, csrf_token) {
+            console.log('111111');
+            $.ajax({
+                 type: 'POST',
+                 url: '/collect/log/',
+                 data: {
+                        "csrfmiddlewaretoken": csrf_token,
+                        "event_type": event_type,
+                        "user_id": user_id,
+                        "content_id": content_id,
+                        "session_id": session_id
+                        },
+                 fail: function(){
+                     console.log('log failed(' + event_type + ')')
+                    }
+})};
--- static/js/movie-loader.js
+++ static/js/movie-loader.js
@@ -32,19 +32,4 @@ function get_movie_data(movie_id, title, api_key) {
     )
 }
 
-function add_impression(user_id, event_type, content_id, session_id, csrf_token) {
-            console.log('111111');
-            $.ajax({
-                 type: 'POST',
-                 url: '/collect/log/',
-                 data: {
-                        "csrfmiddlewaretoken": csrf_token,
-                        "event_type": event_type,
-                        "user_id": user_id,
-                        "content_id": content_id,
-                        "session_id": session_id
-                        },
-                 fail: function(){
-                     console.log('log failed(' + event_type + ')')
-                    }
-})};
+
